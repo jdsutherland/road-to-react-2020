474e52952966530d4bb0345fa360e5a142f03c00
Jeff Sutherland <jeffdsuth@gmail.com>
2020-08-02 19:13:34 -0700

    Implement search

--- src/App.js
+++ src/App.js
@@ -20,7 +20,16 @@ const App = () => {
     },
   ];
 
-  const handleSearch = (event) => console.log(event.target.value);
+  const [searchTerm, setSearchTerm] = useState('')
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value)
+  }
+
+  const searchedStories = stories.filter(story =>
+    story.title
+      .toLowerCase()
+      .includes(searchTerm.toLowerCase()));
 
   return (
     <div>
@@ -30,27 +39,16 @@ const App = () => {
 
       <hr />
 
-      <List list={stories} />
+      <List list={searchedStories} />
     </div>
   );
 }
 
 const Search = ({ onSearch }) => {
-  const [searchTerm, setSearchTerm] = useState('')
-
-  const handleChange = (event) => {
-    setSearchTerm(event.target.value)
-    onSearch(event)
-  };
-
   return (
     <div>
       <label htmlFor="search">Search: </label>
-      <input id="search" type="text" onChange={handleChange} />
-
-        <p>
-        Searching for <strong>{searchTerm}</strong>
-    </p>
+      <input id="search" type="text" onChange={onSearch} />
     </div>
   );
 }
